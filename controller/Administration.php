7562e79066f3eaedbad6be7529c04e67346f25a1
Komche <31341079+Komche@users.noreply.github.com>
2019-08-21 07:47:34 +0100

    definiation des constante

--- controller/Administration.php
+++ controller/Administration.php
@@ -1,3 +1,7 @@
 <?php
 include_once('model/class/RoleManager.php');
 
+function addRoles($url, $data)
+{
+    
+}
---
+++ model/global.php
@@ -0,0 +1,11 @@
+<?php
+$folder = '';
+
+define ('HOST', "http://" . $_SERVER ['HTTP_HOST']); // get the hostname
+
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $folder = '/saroapp';
+}
+
+define('ROOT_PATH', HOST . "$folder");
+define('API_ROOT_PATH', HOST . "$folder/api/object");
