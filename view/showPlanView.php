6c2b023fbb686d2746b11de8a8d407b90edba321
Komche <31341079+Komche@users.noreply.github.com>
2019-09-01 23:18:54 +0100

    ajout du titre

--- view/showPlanView.php
+++ view/showPlanView.php
@@ -42,7 +42,7 @@ ob_start();
         <img class="modal-content" id="img01">
       </span>
       <!-- Descripcion de la imagen -->
-      <div id="caption">hola</div>
+      <div id="caption">SaroApp</div>
     </div>
   </div>
 
