987a84cd7c03cd25c455dd9b88df75f30476fa80
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-04-26 16:19:04 +0100

    afficher equipe

---
+++ var.php
@@ -0,0 +1,55 @@
+<?php
+        //Pour la connexion à la base de donnée
+        $this->config["host"] = '185.98.131.128';
+        $this->config["db_name"] = 'coron1363653';
+        $this->config["username"] = 'coron1363653';
+        $this->config["password_"] = 'qtl1xghbdn';
+$this->config["tables"] = ['actions','candidat','equipe','files','module','module_role','note','projet','region','roles','type_agent','users',];
+
+$this->config['tables']['actions'] = ['id','name','description','action_url','module',];
+
+$this->config['tables']['actions']['id'] = ['id'];
+
+$this->config['tables']['candidat'] = ['id_candidat','nom_candidat','prenom_candidat','email','telephone','role','equipe',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];
+
+$this->config['tables']['equipe'] = ['id_equipe','nom_equipe','region',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];
+
+$this->config['tables']['files'] = ['id','file_name','file_url','file_type','file_size',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];
+
+$this->config['tables']['module'] = ['id','name','icon','description','action_url','sub_module',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];
+
+$this->config['tables']['module_role'] = ['id','role_id','module',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];
+
+$this->config['tables']['note'] = ['id_note','faisabilite','apport','originalite','viabilite','user','projet',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['note']['id'] = ['id_note'];
+
+$this->config['tables']['projet'] = ['id_projet','nom_projet','domaine','file','desciption','statut','besion_tech','etat_retenu','equipe','user',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['note']['id'] = ['id_note'];$this->config['tables']['projet']['id'] = ['id_projet'];
+
+$this->config['tables']['region'] = ['id_region','nom_region',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['note']['id'] = ['id_note'];$this->config['tables']['projet']['id'] = ['id_projet'];$this->config['tables']['region']['id'] = ['id_region'];
+
+$this->config['tables']['roles'] = ['id','name','description',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['note']['id'] = ['id_note'];$this->config['tables']['projet']['id'] = ['id_projet'];$this->config['tables']['region']['id'] = ['id_region'];$this->config['tables']['roles']['id'] = ['id'];
+
+$this->config['tables']['type_agent'] = ['id','label',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['note']['id'] = ['id_note'];$this->config['tables']['projet']['id'] = ['id_projet'];$this->config['tables']['region']['id'] = ['id_region'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_agent']['id'] = ['id'];
+
+$this->config['tables']['users'] = ['id','first_name','last_name','matricule','phone_number','email','type_agent','created_at','updated_at','photo','role','status','password_',];
+
+$this->config['tables']['actions']['id'] = ['id'];$this->config['tables']['candidat']['id'] = ['id_candidat'];$this->config['tables']['equipe']['id'] = ['id_equipe'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['note']['id'] = ['id_note'];$this->config['tables']['projet']['id'] = ['id_projet'];$this->config['tables']['region']['id'] = ['id_region'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_agent']['id'] = ['id'];$this->config['tables']['users']['id'] = ['id'];
---
+++ view/listeProjetView.php
@@ -0,0 +1,66 @@
+<?php
+$title = "Liste des projets" ;
+ob_start();
+?>
+<div class="row">
+  <div class="col-md-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title"><?=$title ?></h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table class="table table-bordered">
+                <tbody>
+                <tr>
+                  <th>Nom du projet</th>
+                  <th>Domaine</th>
+                  <th>Statut</th>
+                  <th>Besoin Tech</th>
+                  <th>Description</th>
+                  <th>Equipe</th>
+                  <th>Retenu</th>
+                  <th>Logo</th>
+                </tr>
+                <?php 
+                  $projet = new projet();
+                  $data = Manager::getDatas($projet)->all();
+                  //print_r($data);
+                  if ((is_array($data) || is_object($data)) && empty($data['message'])) {
+                    foreach ($data as $value) {
+                      
+                   
+                ?>
+                <tr>
+                  <td><?= $value['nom_projet'] ?></td>
+                  <td><?= $value['domaine'] ?></td>
+                  <td><?= $value['statut'] ?></td>
+                  <td><?= $value['besoin_tech'] ?></td>
+                  <td><?= $value['description'] ?></td>
+                  <td><?= Manager::getDatas(new equipe())->getId_equipe($value['equipe'])->getNom_equipe() ?></td>
+                  <td><?= $value['retenu'] ?></td>
+                  <td>
+                  <img width="50" src="<?= Manager::getDatas(new files())->getId($value['photo'])->getFile_url() ?>" class="img-circle" alt="Cinque Terre">
+                  
+                  </td>
+                </tr>
+                <?php 
+                   }
+                  }else {
+                     Manager::messages('Aucune donnée trouvé', 'alert-warning');
+                  }
+                ?>
+              </tbody>
+            </table>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer clearfix">
+              </ul>
+            </div>
+          </div>
+      </div>
+</div>
+<?php
+$content = ob_get_clean();
+require('template.php');
+?>
