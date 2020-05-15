1d746a7c8f1a55d21cf4af18beb0b60ed523fbd6
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-05-15 07:45:12 +0100

    debug cloture inscription

--- public/js/data_handler.js
+++ public/js/data_handler.js
@@ -683,9 +683,8 @@ function canContinue(data) {
         return true;
     }
 }
-
-$('#checkbox_etat').on('change', function () {//Changement de l'etat de l'inscription
-    $my_etat = $(this).val();
+function changeEtat(checkbox) {
+    $my_etat = $(checkbox).val();
     showPleaseWait();
     console.log(myurl + "inscription/id_inscription/1",$(this).prop('checked'));
     if($(this).prop('checked') == false){
@@ -726,4 +725,47 @@ $('#checkbox_etat').on('change', function () {//Changement de l'etat de l'inscri
             }
         });
     }
-});
+}
+// $('#checkbox_etat').on('change', function () {//Changement de l'etat de l'inscription
+//     $my_etat = $(this).val();
+//     showPleaseWait();
+//     console.log(myurl + "inscription/id_inscription/1",$(this).prop('checked'));
+//     if($(this).prop('checked') == false){
+//         $data = JSON.stringify({"etat":"Non"});
+//         $.ajax({
+//             url: myurl + "inscription/id_inscription/1",
+//             type: "PUT",
+//             contentType: 'application/json',
+//             dataType: "json",
+//             data: $data,
+//             success: function (result) {
+//                 console.log(result);
+//                 hidePleaseWait();
+                
+//             },
+//             error: function (xhr, resp, text) {
+//                 // show error to console
+//                 console.log(xhr, resp, text);
+                
+//             }
+//         });
+//     } else{
+//         $data = JSON.stringify({"etat":"Oui"})
+//         $.ajax({
+//             url: myurl + "inscription/id_inscription/1",
+//             type: "PUT",
+//             contentType: 'application/json',
+//             dataType: "json",
+//             data: $data,
+//             success: function (result) {
+//                 console.log(result);  
+//                 hidePleaseWait();              
+//             },
+//             error: function (xhr, resp, text) {
+//                 // show error to console
+//                 console.log(xhr, resp, text);
+                
+//             }
+//         });
+//     }
+// });
